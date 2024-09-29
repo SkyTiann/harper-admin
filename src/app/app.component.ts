d7e93981ca9233fe24e3da08454ae2439d21e0c6
SkyTiann <wohenwunai2020@outlook.com>
2024-09-29 12:14:33 +0800

    init config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenuModule } from 'primeng/menu';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [RouterOutlet, MenuModule],
+  templateUrl: './app.component.html'
 })
-export class AppComponent {
-  title = 'harper-admin';
-}
+export class AppComponent { }
